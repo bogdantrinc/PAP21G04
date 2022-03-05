6eb98dc1034cf1fe2c1f4f254b32b46d7c83a82b
Bogdan <bogdan.trinc@student.upt.ro>
2022-03-05 19:10:43 +0200

    Tema_3

--- C3_Homework.py
+++ C3_Homework.py
@@ -3,7 +3,21 @@
     - afișarea tipului fiecărui element din lista
     - Aflarea numărului numerelor întregi, numerelor float, respectiv a șirurilor din lista
 """
-# todo
+lista = ['abc', 123, '1', 1]
+print(f"{lista[0]}: ", type(lista[0]), f"\n{lista[1]}: ", type(lista[1]), f"\n{lista[2]}: ", type(lista[2]), f"\n{lista[3]}: ", type(lista[3]))
+nr_int = 0
+nr_float = 0
+string = 0
+for i in lista:
+    if str(type(i)) == "<class 'int'>":
+        nr_int += 1
+    if str(type(i)) == "<class 'str'>":
+        string += 1
+    if str(type(i)) == "<class 'float'>":
+        nr_float += 1
+print(f"{nr_int} numere intregi.")
+print(f"{string} siruri.")
+print(f"{nr_float} numere de tip float.")
 
 """
 2. Luati ca input un sir de la utilizator. Transformați șirul într-o lista și 
@@ -12,7 +26,12 @@
     Introduceti un cuvant: Ananas
     Vocale in cuvantul dvs: 3
 """
-# todo
+sir = list(input("Sir: ").lower())
+counter = 0
+for i in sir:
+    if i in 'aeiou':
+        counter += 1
+print("Numarul vocalelor: ", counter)
 
 """
 3. Se cere un script pentru noii vecini într-un cartier. Scriptul:
@@ -26,5 +45,16 @@
    
    - La final se afiseaza mesajul: “Cererea a fost inregistrata. Va multumim.”
 """
-# todo
-
+print("Providerii disponibili sunt: A-Mobile, S5Net, TeleNet.")
+while True:
+    provider = input("Provider de Internet: ")
+    if provider in ["A-Mobile", "S5Net", "TeleNet"]:
+        break
+    print("Utilizatorul nu este disponibil, va rog să alegeți altul.")
+print("Abonamentele disponibile sunt: 500Mbit/s, 1Gb/s, 350Mb/s.")
+while True:
+    abonament = input("Tipul de abonament dorit:")
+    if abonament in ["500Mbit/s", "1Gb/s", "350Mb/s"]:
+        break
+    print("Abonamentul nu este disponibil, va rog să alegeți altul.")
+print("Cererea a fost inregistrata. Va multumim.")
