ad756ce7e905c473b21cbd195d2f516fc99db705
Bogdan <bogdan.trinc@student.upt.ro>
2022-03-05 20:06:11 +0200

    Tema_5

--- C5_Homework.py
+++ C5_Homework.py
@@ -13,48 +13,48 @@
     *** Calcularea varstei se va face într-o funcție separată
 	** Castigul se va extrage cu ajutorul funcției choice() din modulul random
 """
-# from datetime import date
-# from random import choice
-#
-#
-# def verificare_varsta(valoare):
-#     data_nasterii = date.today()
-#     if valoare.isdigit() is False and len(valoare) != 13:
-#         print("CNP invalid!")
-#         return None
-#     if valoare[0] in '12':
-#         data_nasterii = date(int('19' + valoare[1:3]), int(valoare[3:5]), int(valoare[5:7]))
-#     elif valoare[0] in '56':
-#         data_nasterii = date(int('20' + valoare[1:3]), int(valoare[3:5]), int(valoare[5:7]))
-#     else:
-#         return None
-#     if (date.today() - data_nasterii).days < 6575:  # 18 years = 6574.35 days
-#         print("Esti minor, nu poti participa la loterie!")
-#         return None
-#     return data_nasterii
-#
-#
-# def cod_numeric(cnp: str, suma: str):
-#     castiguri = ["un suc", "o punga de chipsuri", "o caserola de prajituri", "un prajitor de paine", "o consola de Gaming", "o tastatura mecanica", "un robot de bucatarie", "o masina"]
-#     varsta = verificare_varsta(cnp)
-#     if varsta is None:
-#         return None
-#     try:
-#         suma = float(suma)
-#     except BaseException:
-#         print("EROARE!, Format incorect.")
-#         return None
-#     if suma < 100:
-#         return " Ai castigat, " + choice(castiguri[:3]) + '! '
-#     elif suma < 500:
-#         return " Ai castigat, " + choice(castiguri[3:6]) + '! '
-#     elif suma > 500:
-#         return " Ai castigat, " + choice(castiguri) + '! '
-#
-#
-# print("    Loteria Bonurilor    ".center(50, '='))
-# print(str(cod_numeric(input("CNP: "), input("Suma: "))).center(50, '-'))
-# print("    La revedere!    ".center(50, '='))
+from datetime import date
+from random import choice
+
+
+def verificare_varsta(valoare):
+    data_nasterii = date.today()
+    if valoare.isdigit() is False and len(valoare) != 13:
+        print("CNP invalid!")
+        return None
+    if valoare[0] in '12':
+        data_nasterii = date(int('19' + valoare[1:3]), int(valoare[3:5]), int(valoare[5:7]))
+    elif valoare[0] in '56':
+        data_nasterii = date(int('20' + valoare[1:3]), int(valoare[3:5]), int(valoare[5:7]))
+    else:
+        return None
+    if (date.today() - data_nasterii).days < 6575:  # 18 years = 6574.35 days
+        print("Esti minor, nu poti participa la loterie!")
+        return None
+    return data_nasterii
+
+
+def cod_numeric(cnp: str, suma: str):
+    castiguri = ["un suc", "o punga de chipsuri", "o caserola de prajituri", "un prajitor de paine", "o consola de Gaming", "o tastatura mecanica", "un robot de bucatarie", "o masina"]
+    varsta = verificare_varsta(cnp)
+    if varsta is None:
+        return None
+    try:
+        suma = float(suma)
+    except BaseException:
+        print("EROARE!, Format incorect.")
+        return None
+    if suma < 100:
+        return " Ai castigat, " + choice(castiguri[:3]) + '! '
+    elif suma < 500:
+        return " Ai castigat, " + choice(castiguri[3:6]) + '! '
+    elif suma > 500:
+        return " Ai castigat, " + choice(castiguri) + '! '
+
+
+print("    Loteria Bonurilor    ".center(50, '='))
+print(str(cod_numeric(input("CNP: "), input("Suma: "))).center(50, '-'))
+print("    La revedere!    ".center(50, '='))
 
 """
 2. Detectati posibilele erori din codul de mai jos care pot apărea în momentul rulării 
